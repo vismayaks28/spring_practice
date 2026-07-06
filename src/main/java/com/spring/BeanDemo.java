005c277c4c9634662b9f39b5f2d7205ef02871b4
Vismayaks28 <vismayaksofficial@gmail.com>
2026-07-06 14:09:14 +0530

    feat: learn spring basics

---
+++ src/main/java/com/spring/BeanDemo.java
@@ -0,0 +1,7 @@
+package com.spring;
+
+public class BeanDemo {
+    public void add(int a , int b){
+        System.out.println("addition: "+ (a+b));
+    }
+}
---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import com.spring.BeanDemo;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ){
+            // spring container its is also known as IOC container
+            //is response to create object and inject it.
+            //in sprint there ate three type of container
+            //beanfactory super container
+            // apllicationContext child container of beanfactory
+            //webapplicationcontext child container of applicationcontext
+            ApplicationContext container = new ClassPathXmlApplicationContext("spring-config.xml");
+    // it will create bean object for beandemo.class
+            BeanDemo bean =  container.getBean(BeanDemo.class);
+            bean.add(4,5);
+
+            BeanDemo d = new BeanDemo();
+            d.add(4,5);
+
+
+
+    }
+}
