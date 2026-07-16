63146b480aff85a9f21b96e6f534ec7a3f5a6ca7
Vismayaks28 <vismayaksofficial@gmail.com>
2026-07-16 11:58:21 +0530

    Understood the singleton class concept and learn how it works via constructor and setter/getter method

--- src/main/java/com/spring/DBConnection.java
+++ src/main/java/com/spring/DBConnection.java
@@ -7,21 +7,45 @@ public class DBConnection {
 
 //    step1:
     //make the constructor private
-    private  DBConnection(String connection, String password, String username) {
+    public  DBConnection(String connection, String password, String username) {
         this.connection = connection;
         this.password = password;
         this.username = username;
     }
-//    step 2:
-    //create a method that will return only 1 object
-    private static DBConnection dbConnection;// by default the value is null
 
-    public static DBConnection getDBConnection(){
-        if(dbConnection == null){
-            dbConnection = new DBConnection("MySQL","root","root");
-
-        }
-        return dbConnection;
-
+    public String getConnection() {
+        return connection;
     }
+
+    public void setConnection(String connection) {
+        this.connection = connection;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    ////    step 2:
+//    //create a method that will return only 1 object
+//    private static DBConnection dbConnection;// by default the value is null
+//
+//    public static DBConnection getDBConnection(){
+//        if(dbConnection == null){
+//            dbConnection = new DBConnection("MySQL","root","root");
+//
+//        }
+//        return dbConnection;
+//
+//    }
 }
--- src/main/java/com/spring/MainClass.java
+++ src/main/java/com/spring/MainClass.java
@@ -1,15 +1,26 @@
 package com.spring;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class MainClass {
     public static void main(String[] args){
-    DBConnection db1 = DBConnection.getDBConnection();
-    DBConnection db2 = DBConnection.getDBConnection();
-   //only one object created
-    System.out.println(db1);
-    System.out.println(db2);
+
+        // constructor
+
+//    DBConnection db1 = DBConnection.getDBConnection();
+//    DBConnection db2 = DBConnection.getDBConnection();
+//   //only one object created
+//    System.out.println(db1);
+//    System.out.println(db2);
 
 
-
+// using setter and getter method
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
+        DBConnection db1 = context.getBean(DBConnection.class);
+        DBConnection db2 = context.getBean(DBConnection.class);
+        System.out.println(db1);
+        System.out.println(db2);
 
 
 
