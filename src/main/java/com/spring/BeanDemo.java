a377da77120e1d67ee0fc50eb787a8261be16602
Vismayaks28 <vismayaksofficial@gmail.com>
2026-07-16 10:58:54 +0530

    initializing the data member via constructor

--- src/main/java/com/spring/BeanDemo.java
+++ src/main/java/com/spring/BeanDemo.java
@@ -1,25 +1,28 @@
 package com.spring;
 //initializing property value via setter method
+// initializing the data member/property values via constructor
 public class BeanDemo {
     private int id;
     private String name;
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
+    //constructor
+    public BeanDemo(int id, String name){
         this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
         this.name = name;
     }
 
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
     public void add(int a , int b){
         System.out.println("addition: "+ (a+b));
     }
